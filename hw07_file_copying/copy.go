a4eeb4f42ab1e0c77b908885fcee4edbc69d828c
Stas Merzyakov <merzliakov@gmail.com>
2024-11-16 13:47:19 +0400

    hw07

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +12,40 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
-	return nil
+	input, err := os.Open(fromPath)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := input.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	if _, err := input.Seek(offset, io.SeekStart); err != nil {
+		panic(err) // offset больше, чем размер файла - невалидная ситуация
+	}
+
+	output, err := os.OpenFile(toPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o666)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := output.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	var inReader io.Reader
+	if limit > 0 {
+		inReader = io.LimitReader(input, limit)
+	} else {
+		inReader = input
+	}
+
+	_, err = io.Copy(output, inReader)
+
+	return err
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,98 @@
 package main
 
-import "testing"
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"os"
+	"path"
+	"path/filepath"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const TestDataDir = "testdata"
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	testCases := []struct {
+		name     string
+		input    string
+		offset   int64
+		limit    int64
+		expected string
+	}{
+		{
+			"offset0_limit0",
+			"input.txt",
+			0,
+			0,
+			"out_offset0_limit0.txt",
+		},
+		{
+			"offset0_limit10",
+			"input.txt",
+			0,
+			10,
+			"out_offset0_limit10.txt",
+		},
+		{
+			"offset0_limit1000",
+			"input.txt",
+			0,
+			1000,
+			"out_offset0_limit1000.txt",
+		},
+		{
+			"offset0_limit10000",
+			"input.txt",
+			0,
+			10000,
+			"out_offset0_limit10000.txt",
+		},
+		{
+			"offset100_limit1000",
+			"input.txt",
+			100,
+			1000,
+			"out_offset100_limit1000.txt",
+		},
+		{
+			"offset6000_limit1000",
+			"input.txt",
+			6000,
+			1000,
+			"out_offset6000_limit1000.txt",
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			fromPath := path.Join(TestDataDir, test.input)
+
+			output := tempFileName("temp_output", ".tmp")
+
+			require.NoError(t, Copy(fromPath, output, test.offset, test.limit))
+
+			expectedFile := path.Join(TestDataDir, test.expected)
+
+			expected, err := os.ReadFile(expectedFile)
+			require.NoError(t, err)
+
+			actual, err := os.ReadFile(output)
+			require.NoError(t, err)
+
+			require.True(t, reflect.DeepEqual(expected, actual))
+			require.NoError(t, os.Remove(output))
+		})
+	}
+}
+
+func tempFileName(prefix, suffix string) string {
+	randBytes := make([]byte, 16)
+	if _, err := rand.Read(randBytes); err != nil {
+		panic(err)
+	}
+
+	return filepath.Join(os.TempDir(), prefix+hex.EncodeToString(randBytes)+suffix)
 }
